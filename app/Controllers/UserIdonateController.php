e9d38ecc0f28a4cef5afa3783908185e794e227a
Changel <delachicachristiaangelicam@gmail.com>
2024-11-17 22:43:14 +0800

    Update UserIdonateController.php

--- app/Controllers/UserIdonateController.php
+++ app/Controllers/UserIdonateController.php
@@ -67,8 +67,8 @@ class UserIdonateController extends BaseController
 
         // Move the uploaded file to a writable location
         $fileName = $file->getRandomName(); // Generate a unique name
-        $filePath = $_SERVER['DOCUMENT_ROOT'] . 'uploads/' . $fileName;
-        $file->move($_SERVER['DOCUMENT_ROOT'] . 'uploads/', $fileName);
+        $filePath = $_SERVER['DOCUMENT_ROOT'] . '/uploads/' . $fileName;
+        $file->move($_SERVER['DOCUMENT_ROOT'] . '/uploads/', $fileName);
 
 
         $this->sendEmailReceived($email, $name, $filePath);
