61663b3506421d2340f8bec68312a5c7b4bc64fb
Changel <delachicachristiaangelicam@gmail.com>
2024-11-24 23:43:34 +0800

    Update ViewController.php

--- app/Controllers/ViewController.php
+++ app/Controllers/ViewController.php
@@ -87,7 +87,7 @@ class ViewController extends BaseController
     }
     public function service()
     {
-        $data = ['service' =>  $this->donation->where('image', 'g11.jpg')->findAll()];
+        $data = ['service' =>  $this->Vm->where('image', 'g11.jpg')->findAll()];
         return view('admin/service', $data);
     }
     public function searchs()
